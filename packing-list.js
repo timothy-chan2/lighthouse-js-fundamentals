03f104395f7deaaa705dc72204a000bc5728efb1
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-09-09 16:54:06 +0000

    Add code with a while loop to loop through packing list array and print to console

--- packing-list.js
+++ packing-list.js
@@ -6,3 +6,10 @@ for (let item in packingList) {
 }
 
 console.log(packingList[3]);
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
