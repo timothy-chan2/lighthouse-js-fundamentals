c62296a2425b8e864438646faf4bfe5a58f786b7
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-09-17 03:26:21 +0000

    Add code for Loopy Lighthouse 2 exercise by modifying Loop Lighthouse code

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -9,3 +9,18 @@ for (let num = 100; num <= 200; num++) {
     console.log(num);
   }
 }
+
+// Loopy Lighthouse 2
+const loopyLighthouse = (range, multiples, words) => {
+  for (let num = range[0]; num <= range[1]; num++) {
+    if (num % multiples[0] === 0 && num % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (num % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (num % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(num);
+    }
+  }
+}
