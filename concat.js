63fb9694fb8096d56c8fcbb4bee1671167e2af1a
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-09-17 03:31:45 +0000

    Add code for Concatenate Arrays assignment to put 2 arrays together

---
+++ concat.js
@@ -0,0 +1,14 @@
+const concat = (arr1, arr2) => {
+  if (arr1 === []) {
+    return arr2;
+  } else if (arr2 === []) {
+    return arr1;
+  } else {
+    return [...arr1, ...arr2];
+  }
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
