ed6e38f8540d235f8849195ecb74ac1dba520abc
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-06 04:11:26 +0000

    Add function to return an array of #s from start to end counting by step

---
+++ range.js
@@ -0,0 +1,20 @@
+const range = (start, end, step) => {
+  if (start === undefined || end === undefined || step === undefined) {
+    return [];
+  } else if (start > end) {
+    return [];
+  } else if (step <= 0) {
+    return [];
+  }
+
+  let arr = [];
+
+  for (let i = start; i <= end; i += step) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
