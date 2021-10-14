485fb9782826762ee5b14dae9fd32d629c34cbe5
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-14 21:56:53 +0000

    Add function that determines the last index of an item in an array

---
+++ last-index-of.js
@@ -0,0 +1,16 @@
+const lastIndexOf = (arr, val) => {
+  let ind = -1;
+
+  for (let i in arr) {
+    if ( arr[i] === val) {
+      ind = i;
+    }
+  }
+  return Number(ind);
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
