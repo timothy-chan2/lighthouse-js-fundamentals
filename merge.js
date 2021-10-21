71360a3da68db46158c13edf33ca53407c79a7aa
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-10-21 03:55:00 +0000

    Add function that returns a sorted array of the numbers from both arrays

---
+++ merge.js
@@ -0,0 +1,9 @@
+const merge = (arr1, arr2) => {
+  let arr3 = [...arr1, ...arr2];
+
+  return arr3.sort((a, b) => { return a - b });
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
